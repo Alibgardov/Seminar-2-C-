b97dc58b9229fa2617dc76d0e2a96523a199c54a
Andrew <alibgardov@mail.ru>
2023-06-13 15:42:54 +0400

    Задачи с семинара 2

---
+++ task12/Program.cs
@@ -0,0 +1,25 @@
+﻿Console.Clear();
+Console.WriteLine();
+int num1 = new Random().Next(11, 101);
+int num2 = new Random().Next(1, 11);
+int result = 0;
+int mnumber(){
+    result = num1 % num2;
+    return result;
+}
+mnumber();
+Console.WriteLine();
+if (result == 0) Console.WriteLine($"Число {num1} кратно  {num2}");
+else{
+    Console.WriteLine($"Число {num1} не кратно {num2}, остаток равен {num1 % num2}");
+}
+Console.WriteLine();
+
+
+/*12 Напишите программу, которая будет принимать на вход два числа и выводить, 
+является ли второе число кратным первому. Если второе число не кратно числу первому,
+то программа выводит остаток от деления.
+
+// 34, 5 -> не кратно, остаток 4
+// 16, 4  -> кратно
+*/
---
+++ task14/Program.cs
@@ -0,0 +1,32 @@
+﻿Console.Clear();
+Console.Write("ВВедите число:");
+int num1 = int.Parse(Console.ReadLine());
+Console.WriteLine();
+int a = 7;
+int b = 23;
+string result;
+if (num1 % a == 0 && num1 % b == 0)  
+result = ("Кратно обоим числам");
+else if (num1 % a != 0 && num1 % b != 0)
+result = ("Не кратно ни одному из чисел");
+else if (num1 % a == 0)
+result = ($"Кратно только числу {a}");
+else result = ($"Кратно только числу {b}");
+
+Console.WriteLine($"Проверить кратность чисел {num1} числам {a} и {b}");
+Console.WriteLine();
+Console.WriteLine($"Числа {num1} {result}");
+Console.WriteLine();
+
+// **Задача 14:**
+
+// Напишите программу, которая принимает на вход число и проверяет,
+//  кратно ли оно одновременно 7 и 23.
+//if((a > b || a < c) && (x > b || x < c) || d == 5) <=> 0 && 1 || d == 5
+//     0    +    0          1    +   0
+ //         0          *         1
+ //                    0                   +   1
+ //                                1    
+// 14  ->  нет
+// 46  ->  нет
+// 161 ->  да
